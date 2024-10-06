b6b7f08b4e011856a7796c4a14dfee70f9894aeb
kyluke mcdougall <=>
2024-10-06 11:59:35 +0200

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -12,42 +12,44 @@ use output::outputter;
 use repository::db::SqliteRepository;
 use std::io::IsTerminal;
 use std::io::{self, Read};
+use crate::args::Args;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = args::Args::parse();
     let repo = SqliteRepository::new("app.db")?;
 
-    if let Some(chat_gpt_api_key) = args.chat_gpt_api_key {
+    if let Some(ref chat_gpt_api_key) = args.chat_gpt_api_key {
         config_service::write_config(
             &repo,
             &ConfigKeys::ChatGptApiKey.to_key(),
             &chat_gpt_api_key,
         )?;
+        return Ok(());
     }
 
     if args.print_config {
-        match config_service::fetch_config(&repo) {
-            Ok(configs) => configs.iter().for_each(|config| {
-                println!("{:} -> {:}", config.key, config.value);
-            }),
-            Err(_) => println!("failed to fetch config"),
-        }
+        return print_config(&repo);
     }
 
-    let input = if !args.data.is_empty() {
-        args.data.join(" ")
-    } else if !io::stdin().is_terminal() {
-        let mut buffer = String::new();
-        io::stdin()
-            .read_to_string(&mut buffer)
-            .expect("Failed to read stdin");
-        buffer.trim().to_string()
-    } else {
-        eprintln!("No input provided. Use positional arguments or pipe data.");
-        std::process::exit(1);
-    };
+    let input = extract_input_or_quit(&args);
+    request_response_from_ai(&repo, &input).await
+}
 
+fn print_config(repo: &SqliteRepository) -> Result<()> {
+    match config_service::fetch_config(&repo) {
+        Ok(configs) => {
+            configs.iter().for_each(|config| println!("{:} -> {:}", config.key, config.value));
+            Ok(())
+        }
+        Err(_) => {
+            println!("failed to fetch config");
+            Ok(())
+        }
+    }
+}
+
+async fn request_response_from_ai(repo: &SqliteRepository, input: &String) -> Result<()> {
     let open_ai_api_key = config_service::fetch_by_key(&repo, &ConfigKeys::ChatGptApiKey.to_key())?;
     let chat_response = match chat(&open_ai_api_key.value, &input).await {
         Ok(response) => response,
@@ -63,6 +65,21 @@ async fn main() -> Result<()> {
         .collect();
 
     outputter::print(output_messages);
-
     Ok(())
+}
+
+fn extract_input_or_quit(args: &Args) -> String {
+    let input = if !args.data.is_empty() {
+        args.data.join(" ")
+    } else if !io::stdin().is_terminal() {
+        let mut buffer = String::new();
+        io::stdin()
+            .read_to_string(&mut buffer)
+            .expect("Failed to read stdin");
+        buffer.trim().to_string()
+    } else {
+        eprintln!("No input provided. Use positional arguments or pipe data.");
+        std::process::exit(1);
+    };
+    input
 }
